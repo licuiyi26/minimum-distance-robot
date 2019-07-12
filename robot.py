f2fa569c9afd17ed76199cccd06bf4ec294e129e
licuiyi26 <licuiyi95@gmail.com>
2019-07-12 21:46:43 +1000

    Change the if/else block by adding an else statement
    - Change the if/else block to check if the current item is any element in the array besides the first one.
    - Add an else statement to check if it is the first element.
    - If the current item starts with "F" or "B", check its directions
    - If the previous item starts with "F" or "B", then skip to next round because they don't need to check the direction; they perform the action.

--- robot.py
+++ robot.py
@@ -5,63 +5,55 @@ from itertools import tee, islice, chain
 # chain links two sequences into one; it's used here to append a single-element sequence [None] to prevs
 # islice is used to make a sequence of all elements except the first, then chain is used to append a None to its end
 
-
 def previous_and_next(movements_array):
     prevs, items, nexts = tee(movements_array, 3)
     prevs = chain([None], prevs)
     nexts = chain(islice(nexts, 1, None), [None])
     return zip(prevs, items, nexts)
 
-
 x, y = 0, 0
 directions = ["N", "E", "S", "W"]
 facing = directions.index("N")  # set the default facing direction of the robot to be north
 
-# set inputs
-# inputs = "F1,R1,B2,L1,B3"
-inputs = input("Please enter a string of commands: e.g. F1,R1,B2,L1,B3")
+inputs = input("Please enter a string of commands （e.g. F1,R1,B2,L1,B3）：")
 movements = re.findall(r"[\w']+", inputs)
 
 for previous, move, nxt in previous_and_next(movements):
-    if previous is None:
+    if previous is not None:
+        if previous[0] == "R":
+            facing = (facing + int(previous[1])) % 4
+        elif previous[0] == "L":
+            facing = (facing - int(previous[1])) % 4
+        else:
+            continue
+
+        if move[0] == "F":
+            if directions[facing] == "N":
+                y += int(move[1])
+            elif directions[facing] == "E":
+                x += int(move[1])
+            elif directions[facing] == "S":
+                y -= int(move[1])
+            elif directions[facing] == "W":
+                x -= int(move[1])
+        elif move[0] == "B":
+            if directions[facing] == "N":
+                y -= int(move[1])
+            elif directions[facing] == "E":
+                x -= int(move[1])
+            elif directions[facing] == "S":
+                y += int(move[1])
+            elif directions[facing] == "W":
+                x += int(move[1])
+
+    else: # means move[0] is the first element of the array
+        facing = 0
         if move[0] == "F":
             y += int(move[1])
         elif move[0] == "B":
             y -= int(move[1])
-        elif move[0] == "R":
-            facing = (0 + int(move[1])) % 4
-        elif move[0] == "L":
-            facing = (0 - int(move[1])) % 4
 
-    else:
-        if move[0] == "R":
-            facing = (facing + int(move[1])) % 4
-        elif move[0] == "L":
-            facing = (facing - int(move[1])) % 4
-
-        if nxt is not None:
-            if facing == 0:
-                if nxt[0] == "F":
-                    y += int(nxt[1])
-                elif nxt[0] == "B":
-                    y -= int(nxt[1])
-            elif facing == 1:
-                if nxt[0] == "F":
-                    x += int(nxt[1])
-                elif nxt[0] == "B":
-                    x -= int(nxt[1])
-            elif facing == 2:
-                if nxt[0] == "F":
-                    y -= int(nxt[1])
-                elif move[0] == "B":
-                    y += int(nxt[1])
-            elif facing == 3:
-                if nxt[0] == "F":
-                    x -= int(nxt[1])
-                elif nxt[0] == "B":
-                    x += int(nxt[1])
-
-
-print (x,y)
+print ("Final coordinates: (" + str(x) + "," + str(y) + ")")
 distance = abs(x) + abs(y)
-print (distance)
+print ("The minimum amount of distance to get back to the starting point: " + str(distance))
+
