7f8f111889552defd0d9db68cb0da8d1dd7d44a9
licuiyi26 <licuiyi95@gmail.com>
2019-07-12 17:06:00 +1000

    Add if/else statements to check the commands
    These two blocks of if/else statements check if the current item contains "R" or "L", and check if it is the last item.

--- robot.py
+++ robot.py
@@ -1,25 +1,47 @@
 import re
 from itertools import tee, islice, chain
 
-def previous_and_next(some_iterable):
-    prevs, items, nexts = tee(some_iterable, 3)
+def previous_and_next(movements_array):
+    prevs, items, nexts = tee(movements_array, 3)
     prevs = chain([None], prevs)
     nexts = chain(islice(nexts, 1, None), [None])
     return zip(prevs, items, nexts)
 
-x,y = 0,0
-facing = 0 # set the default facing direction of the robot to be north
 
-# north = 0
-# east = 1
-# south = 2
-# west = 3
+x, y = 0, 0
+directions = ["N", "E", "S", "W"]
+facing = directions.index("N")  # set the default facing direction of the robot to be north
 
 # set inputs
 inputs = "F1,R1,B2,L1,B3"
 movements = re.findall(r"[\w']+", inputs)
 
 for previous, move, nxt in previous_and_next(movements):
-    print(previous)
-    print(move)
-    print(nxt)
+    if move[0] == "R":
+        facing = (facing + int(move[1])) % 4
+    elif move[0] == "L":
+        facing = (facing - int(move[1])) % 4
+
+    if nxt is not None:
+        if facing == 0:
+            if nxt[0] == "F":
+                y += int(nxt[1])
+            elif nxt[0] == "B":
+                y -= int(nxt[1])
+        elif facing == 1:
+            if nxt[0] == "F":
+                x += int(nxt[1])
+            elif nxt[0] == "B":
+                x -= int(nxt[1])
+        elif facing == 2:
+            if nxt[0] == "F":
+                y -= int(nxt[1])
+            elif move[0] == "B":
+                y += int(nxt[1])
+        elif facing == 3:
+            if nxt[0] == "F":
+                x -= int(nxt[1])
+            elif nxt[0] == "B":
+                x += int(nxt[1])
+
+print (x,y)
