ad9929af9af76a00b25f34be0e8a79617ed3c92d
licuiyi26 <49047782+licuiyi26@users.noreply.github.com>
2019-07-12 15:59:18 +1000

    Add a function called 'previous_and_next'
    This function is to look for the current item in the loop, its previous item and next item.

--- robot.py
+++ robot.py
@@ -1,5 +1,14 @@
-x,y = 0,0 # current direction
-direction = 0 # set the default direction of the robot to be north
+import re
+from itertools import tee, islice, chain
+
+def previous_and_next(some_iterable):
+    prevs, items, nexts = tee(some_iterable, 3)
+    prevs = chain([None], prevs)
+    nexts = chain(islice(nexts, 1, None), [None])
+    return zip(prevs, items, nexts)
+
+x,y = 0,0
+facing = 0 # set the default facing direction of the robot to be north
 
 # north = 0
 # east = 1
@@ -7,8 +16,10 @@ direction = 0 # set the default direction of the robot to be north
 # west = 3
 
 # set inputs
-movements = "F1,R1,B2,L1,B3"
+inputs = "F1,R1,B2,L1,B3"
 movements = re.findall(r"[\w']+", inputs)
 
-for move in movements:
-    print(move[0])
+for previous, move, nxt in previous_and_next(movements):
+    print(previous)
+    print(move)
+    print(nxt)
