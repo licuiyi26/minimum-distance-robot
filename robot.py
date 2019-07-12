63c9a22acf8a5d187bec626a88097219b73234e4
licuiyi26 <licuiyi95@gmail.com>
2019-07-12 22:17:18 +1000

    Add upper() to allow user to enter the command in lowercase
    - Users can enter the command in either lowercase or uppercase.

--- robot.py
+++ robot.py
@@ -1,9 +1,6 @@
 import re
 from itertools import tee, islice, chain
 
-# tee is used to efficiently create 3 independent iterators over the input sequence
-# chain links two sequences into one; it's used here to append a single-element sequence [None] to prevs
-# islice is used to make a sequence of all elements except the first, then chain is used to append a None to its end
 
 def previous_and_next(movements_array):
     prevs, items, nexts = tee(movements_array, 3)
@@ -11,12 +8,13 @@ def previous_and_next(movements_array):
     nexts = chain(islice(nexts, 1, None), [None])
     return zip(prevs, items, nexts)
 
+
 x, y = 0, 0
 directions = ["N", "E", "S", "W"]
 facing = directions.index("N")  # set the default facing direction of the robot to be north
 
 inputs = input("Please enter a string of commands （e.g. F1,R1,B2,L1,B3）：")
-movements = re.findall(r"[\w']+", inputs)
+movements = re.findall(r"[\w']+", inputs.upper())
 
 for previous, move, nxt in previous_and_next(movements):
     if previous is not None:
@@ -46,7 +44,7 @@ for previous, move, nxt in previous_and_next(movements):
             elif directions[facing] == "W":
                 x += int(move[1])
 
-    else: # means move[0] is the first element of the array
+    else:  # means move[0] is the first element of the array
         facing = 0
         if move[0] == "F":
             y += int(move[1])
@@ -55,5 +53,5 @@ for previous, move, nxt in previous_and_next(movements):
 
 print ("Final coordinates: (" + str(x) + "," + str(y) + ")")
 distance = abs(x) + abs(y)
-print ("The minimum amount of distance to get back to the starting point: " + str(distance) + "unit(s)")
+print ("The minimum amount of distance to get back to the starting point: " + str(distance) + " unit(s)")
 
